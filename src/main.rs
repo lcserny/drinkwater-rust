963d636635e39823b5c18d59237208d4e10a7283
Leonardo Cserny <leo.cserny@gmail.com>
2021-11-01 17:28:11 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -9,27 +9,23 @@ pub struct SystemTray {
     #[nwg_control]
     window: nwg::MessageWindow,
 
-    #[nwg_resource(source_file: Some("./test_rc/cog.ico"))]
+    #[nwg_resource(source_file: Some("./assets/glass_original.ico"))]
     icon: nwg::Icon,
 
-    #[nwg_control(icon: Some(& data.icon), tip: Some("Hello"))]
+    #[nwg_control(icon: Some(& data.icon), tip: Some("Drink more water notification app"))]
     #[nwg_events(MousePressLeftUp: [SystemTray::show_menu], OnContextMenu: [SystemTray::show_menu])]
     tray: nwg::TrayNotification,
 
     #[nwg_control(parent: window, popup: true)]
     tray_menu: nwg::Menu,
 
-    #[nwg_control(parent: tray_menu, text: "Hello")]
-    #[nwg_events(OnMenuItemSelected: [SystemTray::hello1])]
-    tray_item1: nwg::MenuItem,
-
-    #[nwg_control(parent: tray_menu, text: "Popup")]
-    #[nwg_events(OnMenuItemSelected: [SystemTray::hello2])]
-    tray_item2: nwg::MenuItem,
+    #[nwg_control(parent: tray_menu, text: "Pause", check: false)]
+    #[nwg_events(OnMenuItemSelected: [SystemTray::pause])]
+    tray_item_pause: nwg::MenuItem,
 
     #[nwg_control(parent: tray_menu, text: "Exit")]
     #[nwg_events(OnMenuItemSelected: [SystemTray::exit])]
-    tray_item3: nwg::MenuItem,
+    tray_item_exit: nwg::MenuItem,
 }
 
 impl SystemTray {
@@ -38,13 +34,24 @@ impl SystemTray {
         self.tray_menu.popup(x, y);
     }
 
-    fn hello1(&self) {
-        nwg::simple_message("Hello", "Hello World!");
+    fn pause(&self) {
+        // TODO: need to use state on struct and another thread
+        if self.tray_item_pause.checked() {
+            self.tray_item_pause.set_checked(false);
+        } else {
+            self.tray_item_pause.set_checked(true);
+        }
+
+        // TODO: this is a notification trigger
+        // self.notify();
     }
 
-    fn hello2(&self) {
+    fn notify(&self) {
         let flags = nwg::TrayNotificationFlags::USER_ICON | nwg::TrayNotificationFlags::LARGE_ICON;
-        self.tray.show("Hello World", Some("Welcome to my application"), Some(flags), Some(&self.icon));
+        self.tray.show("An hour has passed, you need to drink some water!",
+                       Some("Drink Water Notification"),
+                       Some(flags),
+                       Some(&self.icon));
     }
 
     fn exit(&self) {
@@ -53,6 +60,7 @@ impl SystemTray {
 }
 
 fn main() {
+    // TODO: init logging, use something simple
     nwg::init().expect("Failed to init Native Windows GUI");
     let _ui = SystemTray::build_ui(Default::default()).expect("Failed to build UI");
     nwg::dispatch_thread_events();
